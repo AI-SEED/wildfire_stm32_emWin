0219af514a71a74c1518cc35ee0079caf477e69a
flyleaf91 <flyleaf91@163.com>
2014-06-17 09:11:43 +0800

    修改touch驱动

--- 0.emWin_WildfireOS/User/lcd/bsp_touch.h
+++ 0.emWin_WildfireOS/User/lcd/bsp_touch.h
@@ -1,5 +1,4 @@
 
-#if 0
 
 #ifndef __BSP_TOUCH_H
 #define	__BSP_TOUCH_H
@@ -48,44 +47,10 @@ Coordinate *Read_2046_2(void);
 void Palette_draw_point(uint16_t x, uint16_t y);
 uint16_t XPT2046_ReadAdc(uint8_t _ucCh);
 
+void XPT2046_WriteCMD(unsigned char cmd) ;
+unsigned short XPT2046_ReadCMD(void) ;
+
+uint16_t XPT2046_ReadAdc_Fliter(uint8_t _ucCh);
+
+
 #endif /* __BSP_TOUCH_H */
-
-#else
-
-#include "stm32f10x.h"
-	typedef struct
-	{
-		uint16_t usMaxAdc;
-		uint16_t usAdcNowX;
-		uint16_t usAdcNowY;
-	}TOUCH_T;
-
-
-
-extern TOUCH_T g_tTP;
-
-/* 触摸事件 */
-enum
-{
-	TOUCH_NONE = 0,		/* 无触摸 */
-	TOUCH_DOWN = 1,		/* 按下 */
-	TOUCH_MOVE = 2,		/* 移动 */
-	TOUCH_RELEASE = 3	/* 释放 */
-};
-
-/* 供外部调用的函数声明 */
-uint8_t TOUCH_GetKey(int16_t *_pX, int16_t *_pY);
-
-uint8_t TOUCH_InRect(uint16_t _usX, uint16_t _usY,
-uint16_t _usRectX, uint16_t _usRectY, uint16_t _usRectH, uint16_t _usRectW);
-
-void bsp_InitTouch(void);
-void Touch_Init(void);
-
-uint16_t XPT2046_ReadAdc(uint8_t _ucCh);
-
-void TOUCH_Scan(void);
-
-
-#endif
-
